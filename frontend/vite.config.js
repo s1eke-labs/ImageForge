08fa550baefdf014e0a951fed109d9ea8511e169
Sean Fang <silence.boy@live.cn>
2026-05-06 00:33:02 +0800

    chore: 移除 vite Safari 15 构建目标配置

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    target: 'safari15',
-    cssTarget: 'safari15',
-  },
   server: {
     proxy: {
       '/api': 'http://localhost:8020',
